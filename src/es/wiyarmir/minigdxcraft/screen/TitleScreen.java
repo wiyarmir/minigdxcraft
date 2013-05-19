4fa41517ca90b459245aba2162791ed9bfe3215c
Guillermo Orellana <wiyarmir@gmail.com>
2013-05-19 03:53:58 +0200

    quick fix

--- src/es/wiyarmir/minigdxcraft/screen/TitleScreen.java
+++ src/es/wiyarmir/minigdxcraft/screen/TitleScreen.java
@@ -3,7 +3,6 @@ package es.wiyarmir.minigdxcraft.screen;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
